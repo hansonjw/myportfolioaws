767b9b549de4e078bd6208251ab3bdaa52d3ed9c
hansonjw <hansonjw@gmail.com>
2022-09-05 17:54:44 -0500

    Update introduction text, other minor tweaks

--- myportfolio/contentWords.py
+++ myportfolio/contentWords.py
@@ -1,19 +1,22 @@
 wordsuco = [
-"I studied Mechanical Engineering in Boulder, CO.  Here I developed an strong interest in math, computation and using machines to find solutions and insights.",  
-"My time at CU was spent working on Computer Aided Design (CAD), Finite Element Analysis (FEA), and Computation Fluid Dynamics (CFD) on UNIX workstations.",
+"I studied Mechanical Engineering in Boulder, CO.  Here I developed a strong interest in math, computation, and using machines to find solutions and insights.",  
+"My time at CU was spent in the UNIX workstation lab where I was building 3D models in CAD, and performing technical analysis in Finite Element Analysis (FEA) and Computation Fluid Dynamics (CFD).",
 "I didn't quite realize it at the time, but I was developing a stronger interest in the analysis, data, computation, and the machine (computer) than I was in working with mechanical systems.",
+"One of my favorite courses at CU was a Computer Science course where we learned the fundamentals of Data Structures and Algorithms through programming in C++."
 ]
 
 wordsund = [
-"As my career progressed, especially in a manufacturing environment, I received my first exposure to the business side of large scale human endeavors.  My interest in economics as an academic subject was piqued, and I quickly saw that financial and investment acumen were crucial to good decision making and being a good engineer.",
-"Getting an MBA was a logical next step and rounded out some key financial economic areas that couple extremely well with a technical background.",
-"At Notre Dame I found a wonderful community of disciplined like-minded learners and intellectually curious thinkers.",
+"As my career progressed, especially in a manufacturing environment, I received my first exposure to the business side of large scale human endeavors.",
+"My interest in economics as an academic subject was piqued, and I quickly saw that financial and investment acumen were crucial to good decision making and being a good engineer.",
+"Getting an MBA was a logical next step and rounded out some key financial and economic areas that couple extremely well with a technical background.",
+"At Notre Dame I found a wonderful community of disciplined, like-minded learners and intellectually curious thinkers, and was exposed to some of the largest and most successful companies in the world. I am extremely grateful for my experience at Notre Dame.",
 ]
 
 wordsem = [
-"After earning an MBA and I was fortunate to land a role at one of the worlds oldest, largest, and most successful bluechip corporations.  At ExxonMobil I had access some of the richest and largest data sets in all of business, and was tasked with turning that data into insights and information.",
-"Here I learned the tremendous value in clear communication, and the key role well-structured economic analysis plays in making sound investment decisions at a corporate level.",
-"A key passion I discovered at ExxonMobil was turning raw data into actionable and insightful information. Through this I found the power in understanding databases, applied computation, code, and web-development.",
+"After earning an MBA I was fortunate to land a role at one of the worlds oldest, largest, and most successful bluechip corporations.",
+"At ExxonMobil I had access some of the richest sets in business, and was tasked with turning that data into insights.",
+"Here I learned of the tremendous value in clear communication, and the key role well-structured economic analysis plays in making sound investment decisions at a corporate level.",
+"A key passion I discovered at ExxonMobil was turning raw data into insightful information. Here I found the power in understanding databases, applied computation, code, and web-development.",
 ]
 
 wordsuca = [
@@ -23,8 +26,9 @@ wordsuca = [
 "I found a perfect fit with the coding and fullstack web-development course at the Berkeley Extension where I earned a sound foundation in JavaScript and web technologies."
 ]
 
-wordsHome = """
-Thank you for visiting my personal home page.  Here you can find information on me, my skills, my interests, and some of the coding projects I'm working on.  Also, there is a page with contact information if you would like to get in touch with me.  Please have a look around.
-
-I've had quite a career working as an engineer, a business analyst, and a developer.  I've got a diverse backgrounda and a well rounded skillset suitable for this dynamic buisiness environmwent today.
-"""
+wordsHome = [
+"Thank you for visiting my personal home page.  Here you can find information on me, my skills, my interests, and some of the coding projects I'm working on.  Also, there is a page with contact information if you would like to get in touch with me.  Please have a look around.",
+"I am a full stack engineer and developer.  More importantly, I am a lifelong learner with a strong curiosity in technology and how it is being utilized to support human endeavors.",
+"My experience is well-rounded and includes analytics and business analysis at ExxonMobil - one of the world's oldest and most successful bluechip companies. ",
+"I have been fortunate to attend two great academic institutions in the University of Colorado, and the University of Notre Dame.  There I gained a strong foundational technical skillset which includes high level technical computing, as well as university coursework in Computer Science.  This technical skillset is complimented with business skills such as investment analysis, economics, and financial acumen.",
+]
--- myportfolio/pageContent.py
+++ myportfolio/pageContent.py
@@ -29,6 +29,41 @@ footer = {
 
 # pages
 #######
+
+home = {
+    'title': 'Welcome',
+    'quote': {
+        'welcome': """"The fight is won or lost far away from witnesses - behind the lines, in the gym, and out there on the road, long before I dance under those lights." - Muhammad Ali""",
+        'skills': """“What looks like talent is often careful preparation. What looks like skill is often persistent revision.” - James Clear""",
+        'learn':""""You can't connect the dots looking forward; you can only connect them looking backward..." - Steve Jobs"""
+    },
+    'words': wordsHome,
+    'icons': {
+        'python':{'icon':'static/icons/python.svg', 'name':'Python', 'link':'https://www.python.org'},
+        'javascript':{'icon':'static/icons/javascript.svg', 'name':'JavaScript (JS)', 'link':'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+        'mysql':{'icon':'static/icons/mysql.svg','name':'MySQL', 'link':'https://www.mysql.com'},
+        'html':{'icon':'static/icons/html.svg','name':'HTML', 'link':'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+        'css':{'icon':'static/icons/css.svg', 'name':'CSS', 'link':'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+        'flask':{'icon':'static/icons/flask.svg', 'name':'Flask', 'link':'https://flask.palletsprojects.com/en/2.2.x/'},
+        'django':{'icon':'static/icons/django.svg', 'name':'Django', 'link':'https://www.djangoproject.com'},
+        'github':{'icon':'static/icons/github.png', 'name':'GitHub', 'link':'https://github.com'},
+        'heroku':{'icon':'static/icons/heroku.png', 'name':'Heroku', 'link':'https://www.heroku.com/'},
+        # 'wordpress':{'icon':'static/icons/wordpress.svg', 'name':'WordPress', 'link':'https://wordpress.com'},
+        'mongodb':{'icon':'static/icons/mongodb.svg', 'name':'MongoDB', 'link':'https://www.mongodb.com'},
+        'bulma':{'icon':'static/icons/bulma.png', 'name':'Bulma', 'link':'https://bulma.io'},
+        'bootstrap':{'icon':'static/icons/bootstrap.png', 'name':'Bootstrap', 'link':'https://getbootstrap.com'},
+    },
+    'university':{
+        'uco':{'images':'static/uco.png', 'title':'University of Colorado', 'words': wordsuco, 'link':'https://colorado.edu'},
+        'und':{'images':'static/und.png', 'title':'University of Notre Dame', 'words': wordsund, 'link':'https://www.nd.edu'},
+        'em':{'images':'static/em.png', 'title':'ExxonMobil', 'words': wordsem, 'link':'https://www.exxonmobil.com'},
+        'uca':{'images':'static/uca.png', 'title':'University of California, Berkeley Extension', 'words': wordsuca, 'link':'https://extension.berkeley.edu'},
+        
+    },
+    'footer': footer
+}
+
+
 projects = {
     'title':'Projects',
     'quote':"""
@@ -76,53 +111,33 @@ projects = {
 }
 
 
-
-home = {
-    'title': 'Welcome',
-    'quote': {
-        'welcome': """
-        "The fight is won or lost far away from witnesses - behind the lines, in the gym, and out there on the road, long before I dance under those lights." - Muhammad Ali'
-        """,
-        'skills': """
-        “What looks like talent is often careful preparation. What looks like skill is often persistent revision.” - James Clear
-        """,
-        'learn':"""
-        "You can't connect the dots looking forward; you can only connect them looking backward..." - Steve Jobs
-        """
+contact = {
+    'quote': "Please reach out through the links below if you would like to get in touch with me...",
+    'contacts': ['linkedin', 'email', 'github', 'twitter'],
+    'images': {
+        'linkedin':'static/icons/linkedin.png',
+        'email':'static/icons/email.png',
+        'twitter':'static/icons/twitter.png',
+        'github':'static/icons/github.png',
     },
-    'words': wordsHome,
-    'icons': {
-        'python':{'icon':'static/icons/python.svg', 'name':'Python', 'link':'https://www.python.org'},
-        'javascript':{'icon':'static/icons/javascript.svg', 'name':'JavaScript (JS)', 'link':'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
-        'mysql':{'icon':'static/icons/mysql.svg','name':'MySQL', 'link':'https://www.mysql.com'},
-        'html':{'icon':'static/icons/html.svg','name':'HTML', 'link':'https://developer.mozilla.org/en-US/docs/Web/HTML'},
-        'css':{'icon':'static/icons/css.svg', 'name':'CSS', 'link':'https://developer.mozilla.org/en-US/docs/Web/CSS'},
-        'flask':{'icon':'static/icons/flask.svg', 'name':'Flask', 'link':'https://flask.palletsprojects.com/en/2.2.x/'},
-        'django':{'icon':'static/icons/django.svg', 'name':'Django', 'link':'https://www.djangoproject.com'},
-        'github':{'icon':'static/icons/github.png', 'name':'GitHub', 'link':'https://github.com'},
-        'heroku':{'icon':'static/icons/heroku.png', 'name':'Heroku', 'link':'https://www.heroku.com/'},
-        # 'wordpress':{'icon':'static/icons/wordpress.svg', 'name':'WordPress', 'link':'https://wordpress.com'},
-        'mongodb':{'icon':'static/icons/mongodb.svg', 'name':'MongoDB', 'link':'https://www.mongodb.com'},
-        'bulma':{'icon':'static/icons/bulma.png', 'name':'Bulma', 'link':'https://bulma.io'},
-        'bootstrap':{'icon':'static/icons/bootstrap.png', 'name':'Bootstrap', 'link':'https://getbootstrap.com'},
+    'links': {
+        'linkedin':'https://www.linkedin.com/in/justinhanson1/',
+        'email':'hansonjw@gmail.com',
+        'twitter':'https://twitter.com/hansonjw',
+        'github':'https://github.com/hansonjw',
     },
-    'university':{
-        'uco':{'images':'static/uco.png', 'title':'University of Colorado', 'words': wordsuco, 'link':'https://colorado.edu'},
-        'und':{'images':'static/und.png', 'title':'University of Notre Dame', 'words': wordsund, 'link':'https://www.nd.edu'},
-        'em':{'images':'static/em.png', 'title':'ExxonMobil', 'words': wordsem, 'link':'https://www.exxonmobil.com'},
-        'uca':{'images':'static/uca.png', 'title':'University of California, Berkeley Extension', 'words': wordsuca, 'link':'https://extension.berkeley.edu'},
-        
+    'href': {
+        'linkedin':'https://www.linkedin.com/in/justinhanson1/',
+        'email':'mailto:hansonjw@gmail.com',
+        'twitter':'https://twitter.com/hansonjw',
+        'github':'https://github.com/hansonjw',
     },
-    'footer': footer
+    'footer':footer,
 }
-
-
 
 fun = {
     'title':'Interests',
-    'quote' : """
-    "Curiosity has its own reason for existing."\n\t- Albert Einstein
-    """,
+    'quote' : """"Curiosity has its own reason for existing." - Albert Einstein""",
     'locs' : ['maroonbells', 'stream', 'mechazilla','garden', 'starbase','saturn5', 'midland', 'brk', 'pebble', 'carmel', 'cash', 'cashmace'],
     'images' : {
         'garden':'static/garden.jpeg',
@@ -140,17 +155,17 @@ fun = {
     },
     'words' : {
         'garden':["This is the SpaceX 'Rocket Garden' at Starbase where the older rockets are on display for the public.", "Wandering through the SpaceX facility and seeing all the progress they have made was such a fascinating experience.", "There is a lauch complex, as well humongous rocket manufacturing building.  It is incredible how close you can get."],
-        'maroonbells':["This is a picture of the famous 'Marroon Bells' near Aspen, CO.", "Having grown up in Colorado I'll always love and appreciate the beauty of the mountains especially in the summertime."],
+        'maroonbells':["This picture taken earlier this summer and is the famous 'Marroon Bells' just outside of Aspen, CO.", "Having grown up in Colorado I'll always love and appreciate the beauty of the mountains especially in the summertime."],
         'starbase':["Starbase Texas is a technology development facility for SpaceX.  Here they are developing the rockets and technology to go to Mars.", "The 'Starbase' sigh is becoming a popular tourist stop and photo opportunity in Texas. The top of this long sign is tiled in solar panels."],
         'mechazilla':["This is a view from Boca Chica Beach looking back at the SpaceX launch complex.", "The launch and catch structure has been nicknamed 'Mechazilla' and is quite a sight to see.", "In the picture you can also see a heavy booster and a 'Starship' on the ground."],
         'carmel':["The Monterey Penninsula is such a beautiful place.", "In this picture you can see a part of the famous Pebble Beach golf course."],
-        'stream':["In Pfieffer Big Sur State Park we were surrounded by giant redwoods and sequoias (although there aren't any in this photo).", "The trees are indescribable, not to mention hard to capture in a photograph...", "Some day I hope to get up to Redwood National Park."],
+        'stream':["In Pfieffer Big Sur State Park we were surrounded by giant redwoods and sequoias (although there aren't any in this photo).", "The trees are indescribable, not to mention hard to capture in a photograph...",],
         'midland':["In 2015 at ExxonMobil I was assigned the West Texas business unit.", "During this time, the United States became the largest oil producer in the world overtaking Saudia Arabia and Russia.  This U.S. oil production renaissance was driven by development of new fields in West Texas where ExxonMobil is a major player.", "This view of the oil fields is what you see as you fly into Midland.  Each 'pad' is an oil well and the extent to which oil fields cover the landscape left a strong impression on me to this day."],
         'brk':["Studying the writings of Warren Buffett has been one of the most import steps in my development as a business professional.", "Financial literacy and understanding investments has become a passion of mine, and in 2016 I attended the Berkshire Hathaway Annual meeting where I heard Mr. Buffett speak in person.", "Understanding how a kid from Omaha, Nebraska became one of the world's richest and most saavvy businessmen has been such an fortifying area of study for me."],
         'pebble':["Another stunning view of the California coast...the loan cypress.","There is something about the Pacific Ocean and the West Cost, even with less than ideal weather on a day like this",],
         'saturn5':["If you are in Texas and you are interested in the space then the Johnson Space Center near Houston is a must see.", "On display here is a Saturn V rocket cobbled together with remaining parts from the Apollo program and missions. This massive rocket is what is required to propell three men to the moon!"],
-        'cash':['Life can take you down some unexpected avenues.','This cat is named Cash and we found him in the bushes accross the street and we ended up fostering him and working with a local animal rescue.',"Cash was adopted in July and has found a loving home!  That was such a neat and fulfilling experience to be a part of."],
-        'cashmace':['We live in a cat friendly neighborhood and on the same day we met Cash, Mace (larger eared kitten on the right) came to our doorstep. Mace was really affraid of humans, and quite wild in a primal sense.','We ended up fostering him as well and it has been such a joy to see him transform into the friendly and fun loving kitten he has become.','We are currently looking for the right home for him.'],
+        'cash':['Life can take you down some unexpected avenues.','This is Cash and we found him in the bushes accross the street.  We took him in and started working with a local animal rescue.',"Cash was adopted in July and has found a loving home!  Helping Cash find his way in the world was such an unexpected and fulfilling experience."],
+        'cashmace':['We live in a cat friendly neighborhood and on the same day we met Cash, Mace (larger eared kitten on the right) came to our doorstep. Mace was really affraid of humans, and quite wild in a primal sense.','We ended up fostering him as well.  It has been such a joy to see him transform into the friendly and fun loving kitten he has become.','We are currently looking for the right home for him.'],
     },
     'links' : {
         'garden':'https://goo.gl/maps/ao5J71RrhYNk6t8r7',
@@ -181,29 +196,6 @@ fun = {
         'cashmace':'Fort Worth, TX',
     },
     'linktext' : {
-    },
-    'footer':footer,
-}
-
-contact = {
-    'k': ['linkedin', 'email', 'github', 'twitter'],
-    'images': {
-        'linkedin':'static/icons/linkedin.png',
-        'email':'static/icons/email.png',
-        'twitter':'static/icons/twitter.png',
-        'github':'static/icons/github.png',
-    },
-    'links': {
-        'linkedin':'https://www.linkedin.com/in/justinhanson1/',
-        'email':'hansonjw@gmail.com',
-        'twitter':'https://twitter.com/hansonjw',
-        'github':'https://github.com/hansonjw',
-    },
-    'href': {
-        'linkedin':'https://www.linkedin.com/in/justinhanson1/',
-        'email':'mailto:hansonjw@gmail.com',
-        'twitter':'https://twitter.com/hansonjw',
-        'github':'https://github.com/hansonjw',
     },
     'footer':footer,
 }
